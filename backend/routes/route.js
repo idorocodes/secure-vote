a15b60cf891faea8a1532a3e1ccd583f652f88c2
John Amos <idoroyen33@gmail.com>
2026-05-21 20:00:17 +0100

    fixed login route assignment

--- backend/routes/route.js
+++ backend/routes/route.js
@@ -1,10 +1,12 @@
 import homePath from "../controllers/home.js";
 
 import express from "express"
+import login from "../controllers/login.js";
 
 
 const router = express.Router();
 
-router.get("/", homePath);
+router.get("/api/v1/", homePath);
+router.post("/api/v1/login",login)
 
 export default router;
